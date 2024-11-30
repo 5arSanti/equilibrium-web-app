6035cbe72435461e9dfd3637657eca6f3d9bafa2
5arSanti <santiari05@hotmail.com>
2024-11-29 23:15:52 -0500

    New production frontend deployment

--- backend/server.js
+++ backend/server.js
@@ -10,34 +10,32 @@ const app = express();
 const whiteList = [
     // Local
 	"http://localhost:5173",
-	"http://localhost:5173",
 
     // DEV Y QA
 
 	// PROD
-	"https://equilibrium-eges.vercel.app",
-	"https://equilibrium-eges-santiago-arias-projects.vercel.app",
-	"https://equilibrium-eges-n4uaajkdh-santiago-arias-projects.vercel.app",
+	"https://5arsanti.github.io"
 ];
 
-// const options = {
-// 	origin: function (origin, callback) {
-// 		if (whiteList.indexOf(origin) !== -1 || !origin) {
-// 		  	callback(null, true)
-// 		} else {
-// 		  	callback(new Error("Acceso denegado, CORS Error"));
-// 		}
-// 	},
-// 	methods: ["POST", "GET", "DELETE", "PATCH", "OPTIONS"],
-// 	credentials: true,
-// 	allowedHeaders: ["Content-Type", "Authorization", "Access-Control-Allow-Credentials"]
-// }
 const options = {
-	origin: true,
+	origin: function (origin, callback) {
+		if (whiteList.indexOf(origin) !== -1 || !origin) {
+		  	callback(null, true)
+		} else {
+		  	callback(new Error("Acceso denegado, CORS Error"));
+		}
+	},
 	methods: ["POST", "GET", "DELETE", "PATCH", "OPTIONS"],
 	credentials: true,
 	allowedHeaders: ["Content-Type", "Authorization", "Access-Control-Allow-Credentials"]
 }
+
+// const options = {
+// 	origin: true,
+// 	methods: ["POST", "GET", "DELETE", "PATCH", "OPTIONS"],
+// 	credentials: true,
+// 	allowedHeaders: ["Content-Type", "Authorization", "Access-Control-Allow-Credentials"]
+// }
 
 app.use(cors(options));
 
@@ -47,15 +45,6 @@ app.use(express.json());
 app.use(cookieParser());
 
 
-app.get("/", (request, response) => {
-	try {
-
-		return response.send("Hola mundo desde mi servidor");
-	}
-	catch (err) {
-		return response.send("Error en el servidor");
-	}
-})
 
 routerApi(app);
 
